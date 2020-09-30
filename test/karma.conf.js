b0ab12e01231f98d7f961111a8f808de6cfcdad3
Udit Vasu <udit@pm.me>
2020-09-30 09:32:19 +0530

    karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration
 // Generated on Mon Nov 09 2015 18:53:12 GMT+0530 (IST)
-process.env.CHROME_BIN = require('puppeteer').executablePath(); // eslint-disable-line no-process-env
+// process.env.CHROME_BIN = require('puppeteer').executablePath(); // eslint-disable-line no-process-env
 
 module.exports = function (config) {
     var configuration = {
@@ -74,15 +74,15 @@ module.exports = function (config) {
         }
     };
 
-    if (process.env.TRAVIS) { // eslint-disable-line no-process-env
-        configuration.customLaunchers = {
-            chromeTravis: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
-            }
-        };
-        configuration.browsers = ['chromeTravis'];
-    }
+    // if (process.env.TRAVIS) { // eslint-disable-line no-process-env
+    //     configuration.customLaunchers = {
+    //         chromeTravis: {
+    //             base: 'ChromeHeadless',
+    //             flags: ['--no-sandbox']
+    //         }
+    //     };
+    //     configuration.browsers = ['chromeTravis'];
+    // }
 
     // Use `npm run test-browser -- --debug` to debug tests in Chrome console
     if (process.argv[2] === '--debug') {
